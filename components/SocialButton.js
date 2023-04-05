0a1d520aa887c880bcb0653b5f903b205dc104de
sanjana <sanjana.advani@neosoftmail.com>
2023-04-05 11:54:41 +0530

    Form componets added

---
+++ components/SocialButton.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import { Text, View,TouchableOpacity,StyleSheet } from 'react-native'
+import {windowWidth,windowHeight} from './utils/Dimensions'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+const SocialButton=({buttonTitle,
+    btnType,
+    color,
+    backgroundColor,
+    ...rest})=>{
+        let bgColor=backgroundColor;
+    return (
+     <TouchableOpacity {...rest}
+     style={[styles.buttonContainer,{backgroundColor:bgColor}]}
+     >
+        <View style={styles.iconWrapper}> 
+            <FontAwesome style={styles.icon} name={btnType} size={22} color={color}/>
+        </View>
+        <View>
+        <Text style={[styles.buttonText,{color:color}]}>{buttonTitle}</Text>
+        </View>
+        
+     </TouchableOpacity>
+    )
+  
+}
+
+export default SocialButton
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+      marginTop: 10,
+      width: '100%',
+      height: windowHeight / 15,
+      backgroundColor: '#2e64e5',
+      padding: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 3,
+    },
+    buttonText: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      color: '#ffffff',
+      fontFamily: 'Lato-Regular',
+    },
+    iconWrapper: {
+        width: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      icon: {
+        fontWeight: 'bold',
+      },
+      btnTxtWrapper: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+  });
+  
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,8 +4,13 @@ import AntDesign from 'react-native-vector-icons/AntDesign'
 
 const LoginScreen=({navigation})=>{
     return (
-      <View>
-        <Text>Login Screen</Text>
+      <View style={{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+      }}>
+        <AntDesign name="user" size={25} color="#666"/>
+        <Text style={{fontFamily:'Kufam-SemiBoldItalic'}}>Login Screen</Text>
         <Button title="Click here"
         onPress={()=>navigation.navigate('Signup')}  
         >
